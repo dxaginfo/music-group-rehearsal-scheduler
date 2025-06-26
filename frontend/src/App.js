8767cb14e8f467c6bdbed7886cf2f6b96393d0c6
dxaginfo <142343166+dxaginfo@users.noreply.github.com>
2025-06-25 23:14:19 -0300

    Initial React App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function App() {
+  return (
+    <div>
+      <h1>Music Group Rehearsal Scheduler</h1>
+      <p>Streamlining rehearsal scheduling and reminders for bands and music groups.</p>
+    </div>
+  );
+}
+export default App;
